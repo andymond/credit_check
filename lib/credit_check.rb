8bdf229570c6c495484f5a28a06fe5d04d8ac386
Andrew Dymond <andy.dymond@gmail.com>
2018-03-18 20:20:43 -0600

    Define credit check class & initialize with card number

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,43 +1,9 @@
-card_number = "342804633855673"
-digits = card_number.reverse.split("")
+class CreditCheck
 
+  attr_reader :card_number
 
-def alternate_doubler(digits)
-  doubled_digits = []
-  digits.each_with_index do |digit, index|
-    if index.odd?
-      doubled_digits << digit.to_i * 2
-    else
-      doubled_digits << digit.to_i
-    end
+  def initialize(card_number)
+    @card_number
   end
-  doubled_digits
-end
-#p doubled_digits
 
-def double_digit_converter(digits)
-  single_digits = alternate_doubler(digits).map do |digit| #probably better to pass output of alternate_doubler to converter
-    if digit > 9
-      digit - 9
-    else
-      digit
-    end
-  end
-  p single_digits
-end
-
-final = double_digit_converter(digits)
-#sum = 0
-#single_digits.each do |digit|
-#  sum += digit
-#end
-
-sum = final.inject(:+)
-p sum
-
-
-if sum % 10 == 0
- p "The number is valid!"
-else
- p "The number is invalid!"
 end
