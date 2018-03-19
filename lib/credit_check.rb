e9ea07b61ba89738a7186a5ae9a1f01bc646d45a
Andrew Dymond <andy.dymond@gmail.com>
2018-03-18 21:01:06 -0600

    add digit conversion methods to CreditCheck

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,9 +1,23 @@
 class CreditCheck
-
   attr_reader :card_number
 
   def initialize(card_number)
-    @card_number
+    @card_number = card_number
   end
 
+  def reverse_digits
+    card_number.split("").reverse.map { |d| d.to_i }
+  end
+
+  def double_alternates
+    reverse_digits.map.with_index do |d, i|
+      d % 2 == 0 ? d * 2 : d
+    end
+  end
+
+  def convert_to_single_digits
+    double_alternates.map do |d|
+      d > 9 ? d - 9 : d
+    end
+  end
 end
